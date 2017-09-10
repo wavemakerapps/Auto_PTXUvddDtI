476f3249e8c79460e4a32a339e2c9eb4769e6262
QmLbNkUbWQ <automate1@wavemaker.com>
2017-09-10 22:15:37 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_PTXUvddDtI",
+  "homePage" : "Main",
+  "name" : "Auto_PTXUvddDtI",
+  "platformType" : "DEFAULT",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
